4fb6a2281a68d0626586d5cffe17a063035139bf
6thanh <sauthanh106@gmail.com>
2022-03-08 05:33:07 +0700

    demo content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Content from './layoutDemo/Content';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world!
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={<Home />} /> */}
+          {/* <Route path='/caseStudy_RJ2111R1_Ha' element={<Home />} /> */}
+          <Route path='/' element={<Content />} />
+          {/* <Route path='/products' element={<Products />} />
+          <Route path='/products/edit/:productId' element={<Edit />} />
+          <Route path='/products/delete/:productId' element={<Delete />} />
+          <Route path='/products/add' element={<Add />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
